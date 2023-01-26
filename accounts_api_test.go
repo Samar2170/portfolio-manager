0a83f7c0e6adb951adce42f7e771e8aaefbf2f33
samar2170 <scipioresearch2020@gmail.com>
2023-01-26 22:02:21 +0530

    going on

--- accounts_api_test.go
+++ accounts_api_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 // type handler struct {
@@ -27,24 +28,51 @@ import (
 type LoginResponse struct {
 	Token string
 }
+type TestSuite struct {
+	suite.Suite
+	Token string
+}
 
-func TestLogin(t *testing.T) {
+func (ts TestSuite) TestLogin() error {
 	e := echo.New()
 	f := make(url.Values)
 	f.Set("username", "Jon Snow")
 	f.Set("password", "test@123")
-	// f.Set("email", "test@gmail.com")
 	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(f.Encode()))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
 	var resp LoginResponse
-	if assert.NoError(t, login(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
+	if assert.NoError(ts.Suite.T(), login(c)) {
+		assert.Equal(ts.Suite.T(), http.StatusOK, rec.Code)
 
-		_ = json.Unmarshal(rec.Body.Bytes(), &resp)
-		fmt.Println(resp)
+		err := json.Unmarshal(rec.Body.Bytes(), &resp)
+		if err != nil {
+			return err
+		}
+		ts.Token = resp.Token
+		assert.Greater(ts.Suite.T(), len(ts.Token), 0)
+	}
+	return nil
+}
+
+func (ts TestSuite) TestHelloWorld() error {
+	token := ts.Token
+	fmt.Println(token)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	if assert.NoError(ts.Suite.T(), hello(c)) {
+		assert.Equal(ts.Suite.T(), http.StatusOK, rec.Code)
+		fmt.Println(rec.Body)
 	}
 
+	return nil
+}
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
 }
